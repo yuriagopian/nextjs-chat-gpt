b81e85c020e47cbe08a55d89265f6ee2674dd086
yuriagopian <yuri.agopian@hotmail.com>
2023-04-22 15:28:19 -0300

    chore: prisma schame initiation

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,7 +1,33 @@
+import { prisma } from "@/app/prisma/prisma";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest){
-  const body = await request.json()
-  return NextResponse.json({body})
+  const body = await request.json();
+  const chatCreated = await prisma.chat.create({
+    data: {
+      user_id: '1',
+      messages: {
+        create: {
+          content: body.message
+        }
+      }
+    },
+    include: {
+      messages: true
+    }
+  })
+
+  return NextResponse.json(chatCreated)
+
+}
+export async function GET(request: NextRequest){
+  const chatCreated = await prisma.chat.findMany({
+    orderBy: {
+      created_at: 'desc'
+    }
+  })
+
+  return NextResponse.json(chatCreated)
 
 }
